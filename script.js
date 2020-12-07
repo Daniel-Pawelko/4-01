9c6e7ac9d2b97245e26a320a496c088ec24ca564
DanielPawelko <daniel.pawelko@ocsbstudent.ca>
2020-12-07 15:00:42 +0000

    s

---
+++ script.js
@@ -0,0 +1,6 @@
+alert( 'Hello, World!' );
+function myFunction() {
+  alert( 'You have clicked the button!' )
+  document.getElementById("a").innerHTML = "Hello World";
+}
+console.log( 'Hello, World!' );
